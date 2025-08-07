922ce4fce5f11cf977281243bd4992b00f57af20
hughfuchsen <hughfuchsen@gmail.com>
2025-08-07 10:06:47 +1000

    .

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,13 +10,22 @@ function Home() {
         <p className="flex justify-start items-start text-3xl underline" style={{ fontWeight: "bold", fontStyle: "italic" }}>
         picapsso’s diary 1997</p>
         <br />
-        <details>
-          <summary>open my cupboards</summary>
-          <ul>
-            <li>live music illustrations</li>
-            <li>writings</li>
+        <details className="p-2">
+          <summary className="cursor-pointer">open my cupboards</summary>
+          <ul className="ml-6 mt-2 space-y-1 list-disc">
+            <li>
+              <a href="/live-music-illustrations" className="underline hover:no-underline">
+                live music illustrations
+              </a>
+            </li>
+            <li>
+              <a href="/writings" className="underline hover:no-underline">
+                writings
+              </a>
+            </li>
           </ul>
         </details>
+
       </div>
 
       {/* <div className="grid grid-cols-5 gap-4">
