a23b42834cc8e2c946d4fae41b9fb5ff97f59fd6
hughfuchsen <hughfuchsen@gmail.com>
2025-08-20 16:36:58 +1000

    .

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()], // combine both in a single array
+  plugins: [react()],
   build: {
     outDir: 'dist'
   }
