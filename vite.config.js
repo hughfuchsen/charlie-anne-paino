c3d3c5d7dfa396a0deb7bc99cb8031172eb2ccca
hughfuchsen <hughfuchsen@gmail.com>
2025-08-20 16:34:47 +1000

    .

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,7 @@ import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  plugins: [svgr()],
+  plugins: [react(), svgr()], // combine both in a single array
   build: {
     outDir: 'dist'
   }
