a21a27552c695800988328f482e68d0cae1ea549
hughfuchsen <hughfuchsen@gmail.com>
2025-09-03 15:03:53 +1000

    .

--- studio-charlie-anne-paino/schemaTypes/index.js
+++ studio-charlie-anne-paino/schemaTypes/index.js
@@ -1,6 +1,12 @@
-import writings from './writings'
-import image from './illustrations';
+// import writings from './writings'
+// import image from './illustrations';
 
-export const schemaTypes = [writings, image]
+// export const schemaTypes = [writings, image]
 
   
+import writings from './writings'
+import illustrations from './illustrations'
+
+export const schema = {
+  types: [writings, illustrations],
+}
